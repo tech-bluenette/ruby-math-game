3af84bc07e82d6a8b1ab5bbc52dd2460a0ab4ba6
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-19 18:22:02 +0000

    update

--- mathgame.rb
+++ mathgame.rb
@@ -1,81 +1,81 @@
-var player1Lives = 3;
-var player2Lives = 3;
-var currentPlayer = 1;
+player1_lives = 3
+player2_lives = 3
+current_player = 1
  
-function checkLives(){
-  if ((player1Lives > 0) && (player2Lives > 0)) {
-    return true;
-  } else {
-    return false;
-  }
-}
- 
-function getAnswer(operator, x, y) {
-  if (operator == 1) {
-    operator = " + ";
-  } else if (operator == 2) {
-    operator = " - ";
-  } else {
-    operator = " x ";
-  }
- 
-  return prompt("Player " + currentPlayer + ": What does " + x + operator + y + " equal?") ;
-}
- 
-function switchPlayer() {
-  if (currentPlayer == 1) {
-    currentPlayer = 2;
-    alert("You have " + player1Lives + " lives remaining");
-  } else {
-    currentPlayer = 1;
-    alert("You have " + player2Lives + " lives remaining");
-  }
-}
- 
-function calculateAnswer(op, x, y) {
-  if (op == 1) {
-    return x + y;
-  } else if (op == 2) {
-    return x - y;
-  } else {
-    return x * y;
-  }
-}
- 
-function announceWinner() {
-  if (player1Lives == 0) {
-    alert("Player 2 wins!");
-  } else {
-    alert("Player 1 wins!");
-  }
-}
- 
-function checkAnswer(answer, rightAnswer) {
-  if (answer == rightAnswer) {
-    alert("Correct.");
-  } else {
-    if (currentPlayer == 1) {
-      player1Lives -= 1;
-      alert("Wrong! " + player1Lives + " lives left!");
-    } else {
-      player2Lives -= 1;
-      alert("Wrong! " + player2Lives + " lives left!");
-    }
-  }
-}
- 
-do {
- 
-  var x  = Math.floor(Math.random() * 20) + 1;
-  var y  = Math.floor(Math.random() * 20) + 1;
-  var op = Math.floor(Math.random() * 3)  + 1;
+function checkLives()
+  if player1_lives > 0 && (player2_lives > 0
+    return true
+    else 
+    return false
   
-  var rightAnswer = calculateAnswer(op, x, y);
-  var answer = getAnswer(op, x, y);
+
  
-  checkAnswer(answer, rightAnswer);
-  switchPlayer();
+function get_answer(operator, x, y) 
+  if operator == 1) 
+    operator = " + "
+    elsif (operator == 2
+    operator = " - "
+    else 
+    operator = " x "
+  
  
-} while (checkLives() == true )
+  return gets.chomp("Player " + current_player + ": What does " + x + operator + y + " equal?") 
+
  
-announceWinner();
+function switch_player() 
+  if current_player == 1
+    current_player = 2
+    gets.chomp "You have " + player1_lives + " lives remaining"
+    else 
+    currentPlayer = 1
+    gets.chomp "You have " + player2_lives + " lives remaining"
+  
+
+ 
+function calculate_answer(op, x, y) 
+  if op == 1) 
+    return x + y
+    elsif (op == 2) 
+    return x - y
+    else 
+    return x * y
+  
+
+ 
+function announce_winner() 
+  if (player1_lives == 0) 
+    gets.chomp "Player 2 wins!"
+    else 
+    gets.chomp "Player 1 wins!"
+  
+
+ 
+function check_answer(answer, right_answer) 
+  if (answer == right_answer) 
+    gets.chomp "Correct."
+    else 
+    if (current_player == 1) 
+      player1_lives -= 1
+      gets.chomp "Wrong! " + player1_lives + " lives left!"
+      else 
+      player2Lives -= 1
+      gets.chomp "Wrong! " + player2_lives + " lives left!"
+    
+  
+
+ 
+do 
+ 
+  x  = Math.floor(Math.random() * 20) + 1
+  y  = Math.floor(Math.random() * 20) + 1
+  op = Math.floor(Math.random() * 3)  + 1
+  
+  right_answer = calculate_answer(op, x, y)
+  answer = get_answer(op, x, y)
+ 
+  check_answer(answer, right_answer)
+  switch_player()
+  
+ while check_lives() == true 
+ 
+announce_winner()
