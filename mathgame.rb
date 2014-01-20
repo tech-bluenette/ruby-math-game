1e03b9290b9452ae07367a07fc34b768ae267ec7
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-20 01:13:32 +0000

    update

--- mathgame.rb
+++ mathgame.rb
@@ -1,71 +1,82 @@
-player1_lives = 3
-player2_lives = 3
-current_player = 1
+@player1_lives = 3
+@player2_lives = 3
+@current_player = 1
  
-function checkLives()
-  if player1_lives > 0 && (player2_lives > 0
+def checkLives()
+  if @player1_lives > 0 && @player2_lives > 0
     return true
-    else 
+  else 
     return false
+  end
+end
   
 
  
-function get_answer(operator, x, y) 
-  if operator == 1) 
+def get_answer(operator, x, y) 
+  if operator == 1
     operator = " + "
-    elsif (operator == 2
+  elsif operator == 2
     operator = " - "
-    else 
+  else 
     operator = " x "
+  end
+end
   
  
-  return gets.chomp("Player " + current_player + ": What does " + x + operator + y + " equal?") 
+  return gets.chomp("Player " + @current_player + ": What does " + x + operator + y + " equal?") 
 
  
-function switch_player() 
-  if current_player == 1
-    current_player = 2
-    gets.chomp "You have " + player1_lives + " lives remaining"
-    else 
-    currentPlayer = 1
-    gets.chomp "You have " + player2_lives + " lives remaining"
+def switch_player() 
+  if @current_player == 1
+    @current_player = 2
+    puts "You have " + @player1_lives + " lives remaining"
+  else 
+    @currentPlayer = 1
+    puts "You have " + @player2_lives + " lives remaining"
+  end
+end
   
 
  
-function calculate_answer(op, x, y) 
-  if op == 1) 
+def calculate_answer(op, x, y) 
+  if op == 1
     return x + y
-    elsif (op == 2) 
+    elsif op == 2
     return x - y
     else 
     return x * y
+  end
+end
+
+ 
+def announce_winner() 
+  if @player1_lives == 0
+    puts "Player 2 wins!"
+  else 
+    puts "Player 1 wins!"
+  end
+end
   
 
  
-function announce_winner() 
-  if (player1_lives == 0) 
-    gets.chomp "Player 2 wins!"
+def check_answer(answer, right_answer) 
+  if answer == right_answer 
+    puts "Correct."
+  else 
+    if (@current_player == 1) 
+      @player1_lives -= 1
+      puts "Wrong! " + @player1_lives + " lives left!"
     else 
-    gets.chomp "Player 1 wins!"
-  
-
- 
-function check_answer(answer, right_answer) 
-  if (answer == right_answer) 
-    gets.chomp "Correct."
-    else 
-    if (current_player == 1) 
-      player1_lives -= 1
-      gets.chomp "Wrong! " + player1_lives + " lives left!"
-      else 
-      player2Lives -= 1
-      gets.chomp "Wrong! " + player2_lives + " lives left!"
+      @player2Lives -= 1
+      puts "Wrong! " + @player2_lives + " lives left!"
+    end
+  end
+end
     
   
 
  
-do 
- 
+ loop do
   x  = Math.floor(Math.random() * 20) + 1
   y  = Math.floor(Math.random() * 20) + 1
   op = Math.floor(Math.random() * 3)  + 1
@@ -76,6 +87,7 @@ do
   check_answer(answer, right_answer)
   switch_player()
   
- while check_lives() == true 
+ break if check_lives() == true 
+end
  
 announce_winner()
