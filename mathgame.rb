14d09440134081ed21d596b2b8b9aa8b7649daec
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-20 05:30:28 +0000

    done

--- mathgame.rb
+++ mathgame.rb
@@ -2,7 +2,7 @@
 @player2_lives = 3
 @current_player = 1
  
-def checkLives()
+def check_lives()
   if @player1_lives > 0 && @player2_lives > 0
     return true
   else 
@@ -20,19 +20,19 @@ def get_answer(operator, x, y)
   else 
     operator = " x "
   end
-end
   
- 
-  return gets.chomp("Player " + @current_player + ": What does " + x + operator + y + " equal?") 
+  puts "Player #{@current_player} What does #{x} #{operator} #{y} equal?"
+  return gets.chomp.to_i
+end
 
  
 def switch_player() 
   if @current_player == 1
     @current_player = 2
-    puts "You have " + @player1_lives + " lives remaining"
+    puts "You have #{@player1_lives} lives remaining"
   else 
     @currentPlayer = 1
-    puts "You have " + @player2_lives + " lives remaining"
+    puts "You have #{@player2_lives} lives remaining"
   end
 end
   
@@ -65,10 +65,10 @@ def check_answer(answer, right_answer)
   else 
     if (@current_player == 1) 
       @player1_lives -= 1
-      puts "Wrong! " + @player1_lives + " lives left!"
+      puts "Wrong! #{@player1_lives} lives left!"
     else 
-      @player2Lives -= 1
-      puts "Wrong! " + @player2_lives + " lives left!"
+      @player2_lives -= 1
+      puts "Wrong! #{@player2_lives} lives left!"
     end
   end
 end
@@ -77,17 +77,18 @@ end
 
  
  loop do
-  x  = Math.floor(Math.random() * 20) + 1
-  y  = Math.floor(Math.random() * 20) + 1
-  op = Math.floor(Math.random() * 3)  + 1
-  
+  x  = Random.new.rand(1..20)
+  y  = Random.new.rand(1..20)
+  op = Random.new.rand(1..3)
+
   right_answer = calculate_answer(op, x, y)
   answer = get_answer(op, x, y)
  
   check_answer(answer, right_answer)
   switch_player()
   
- break if check_lives() == true 
+ break if check_lives() == false 
+
 end
  
 announce_winner()
